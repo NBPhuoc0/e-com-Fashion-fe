184c19f29e9595a1b5583ed207d8fc812dfb993f
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2024-10-25 17:36:23 +0700

    create: checkout page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import BannerSlider from "@/components/banner-slider/BannerSlider";
 import PostList from "@/components/post/PostList";
@@ -6,53 +6,56 @@ import ListProduct from "@/components/product/ListProduct";
 import { Col, Row, Tabs } from "antd";
 import { useEffect, useState } from "react";
 
-import "../styles/Home.custom.css"
-import { jacketProductsHome, jeansProductsHome } from "@/dummy-data/product.data";
+import "../styles/Home.custom.css";
+import {
+  jacketProductsHome,
+  jeansProductsHome,
+} from "@/dummy-data/product.data";
 import Link from "next/link";
 import ProductRecommended from "@/components/product/ProductRecommended";
 
 export default function Home() {
   const banners: string[] = [
-    '/assets/images/banner_1.webp',
-    '/assets/images/banner_2.png',
-    '/assets/images/banner_3.png',
-    '/assets/images/banner_4.webp',
-  ]
+    "/assets/images/banner_1.webp",
+    "/assets/images/banner_2.png",
+    "/assets/images/banner_3.png",
+    "/assets/images/banner_4.webp",
+  ];
 
   const banner_collections: string[] = [
-    '/assets/images/banner_collection_1.webp',
-    '/assets/images/banner_collection_2.webp',
-    '/assets/images/banner_collection_3.webp',
-    '/assets/images/banner_collection_4.webp'
-  ]
+    "/assets/images/banner_collection_1.webp",
+    "/assets/images/banner_collection_2.webp",
+    "/assets/images/banner_collection_3.webp",
+    "/assets/images/banner_collection_4.webp",
+  ];
 
   const categorys = [
     {
       id: 1,
-      name: 'Áo Gió',
-      listProduct: jacketProductsHome
+      name: "Áo Gió",
+      listProduct: jacketProductsHome,
     },
     {
       id: 2,
-      name: 'Jeans Flex',
-      listProduct: jeansProductsHome
+      name: "Jeans Flex",
+      listProduct: jeansProductsHome,
     },
     {
       id: 3,
-      name: 'Áo Polo',
-      listProduct: []
+      name: "Áo Polo",
+      listProduct: [],
     },
     {
       id: 4,
-      name: 'Quần Âu',
-      listProduct: []
+      name: "Quần Âu",
+      listProduct: [],
     },
     {
       id: 5,
-      name: 'Sơ Mi',
-      listProduct: []
+      name: "Sơ Mi",
+      listProduct: [],
     },
-  ]
+  ];
 
   const [isCentered, setIsCentered] = useState(false);
 
@@ -67,19 +70,21 @@ export default function Home() {
 
     handleResize();
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
     <main>
-      <div className='sectionContainer'>
+      <div className="sectionContainer">
         <BannerSlider banners={banners} />
         <div className="mt-6 tabsHome">
-          <h2 className="text-2xl md:text-3xl text-center py-2">Sản phẩm ưa chuộng</h2>
+          <h2 className="text-2xl md:text-3xl text-center py-2">
+            Sản phẩm ưa chuộng
+          </h2>
           <Tabs
             defaultActiveKey="1"
             centered={isCentered}
@@ -94,36 +99,47 @@ export default function Home() {
                       <ListProduct listProduct={category.listProduct} />
                       {category.listProduct.length > 0 && (
                         <Link
-                          href={'/collection/' + category.id}
+                          href={"/collection/" + category.id}
                           className="text-black hover:text-gray-500 font-medium rounded-lg p-2 w-96 border border-gray-600 hover:border-gray-500 mt-5 text-center"
-                        > Xem thêm </Link>
+                        >
+                          {" "}
+                          Xem thêm{" "}
+                        </Link>
                       )}
                     </div>
                   </>
-                )
+                ),
               };
             })}
           />
         </div>
 
         <div className="w-full my-5">
-          <img loading="lazy" src="/assets/images/banner_ads.webp" alt="banner" className="w-full h-full object-cover"/>
+          <img
+            loading="lazy"
+            src="/assets/images/banner_ads.webp"
+            alt="banner"
+            className="w-full h-full object-cover"
+          />
         </div>
 
-        <ProductRecommended/>
+        <ProductRecommended />
 
         <Row gutter={16}>
-          {
-            banner_collections.map((img, index) => {
-              return (
-                <Col span={12} key={index}>
-                    <div className="w-full mb-4">
-                      <img loading="lazy" src={img} alt="collection banner" className="w-full h-full"/>
-                    </div>
-                </Col>
-              )
-            })
-          }
+          {banner_collections.map((img, index) => {
+            return (
+              <Col span={12} key={index}>
+                <div className="w-full mb-4">
+                  <img
+                    loading="lazy"
+                    src={img}
+                    alt="collection banner"
+                    className="w-full h-full"
+                  />
+                </div>
+              </Col>
+            );
+          })}
         </Row>
       </div>
 
