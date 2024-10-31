eb3a766d2357151779a96ef69cdcfbb7536eb35e
Trịnh Hoàng Phúc <96382380+phuc0201@users.noreply.github.com>
2024-10-31 20:07:37 +0700

    update: product slider and number spinner (#17)
    * create: checkout page
    
    * refactor: header comp
    
    * Remove: remove header file
    
    * refactor: Footer comp
    
    * remove: remove sectionContainer class  in ListProduct
    
    * create: promotion drawer in checkout page
    
    * update: responsive homepage
    
    * update: update product slider and numberSpinner

--- src/components/CopyCodeButton.tsx
+++ src/components/CopyCodeButton.tsx
@@ -1,27 +1,30 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const CopyCodeButton = ({ code }: { code: string }) => {
-    const [copied, setCopied] = useState<boolean>(false);
+  const [copied, setCopied] = useState<boolean>(false);
 
-    const copyToClipboard = async () => {
-        try {
-            await navigator.clipboard.writeText(code);
-            setCopied(true);
-            setTimeout(() => setCopied(false), 2000); // Hiển thị thông báo sao chép trong 2 giây
-        } catch (error) {
-            console.error('Failed to copy: ', error);
-        }
-    };
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(code);
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000); // Hiển thị thông báo sao chép trong 2 giây
+    } catch (error) {
+      console.error("Failed to copy: ", error);
+    }
+  };
 
-    return (
-        <div>
-            <button className='bg-amber-500 py-1.5 px-5 text-xs rounded-lg' onClick={copyToClipboard}>
-                {copied ? 'Đã copy!' : 'Copy mã'}
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        className="bg-amber-500 py-1.5 px-5 text-xs rounded-md"
+        onClick={copyToClipboard}
+      >
+        {copied ? "Đã copy!" : "Copy mã"}
+      </button>
+    </div>
+  );
 };
 
 export default CopyCodeButton;
--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,51 +1,70 @@
-'use client'
+"use client";
 
-import { ProductCardModel } from '@/models/product/productCard.model'
-import { Checkbox } from 'antd'
-import React, { useState } from 'react'
-import NumberSpinner from '../NumberSpinner'
-import ModalProduct from './ModalProduct'
+import { ProductCardModel } from "@/models/product/productCard.model";
+import { Checkbox } from "antd";
+import React, { useState } from "react";
+import NumberSpinner from "../NumberSpinner";
+import ModalProduct from "./ModalProduct";
 
 export default function CartItem({ product }: { product: ProductCardModel }) {
-    const [countProduct, setCountProduct] = useState<number>();
-    return (
-        <div className='p-4 bg-white'>
-            <div className='flex gap-4 py-3'>
-                <Checkbox />
-                <div className='flex w-full gap-6'>
-                    <img className='w-16 xl:w-28' loading="lazy" src={product.images[0].url} alt="" />
-                    <div className='flex flex-col gap-3 w-full'>
-                        <div className='flex flex-col gap-2'>
-                            <div className='text-xs lg:text-base line-clamp-1 font-medium text-[#1c2430]'>{product.name}</div>
-                            <div className='flex text-xs lg:text-base gap-3'>
-                                <span className={`font-medium ${product.promotion_price != 0 ? 'text-[#e14337]' : 'text-[#1c2430]'}`}>
-                                    {(product.min_variant_price - product.promotion_price).toLocaleString("vi-VN", {
-                                        style: "currency",
-                                        currency: "VND",
-                                    })}
-                                </span>
-                                {product.promotion_price != 0 &&
-                                    <span className='font-medium text-[#9fb0c7] line-through'>
-                                        <span className=''>
-                                            {product.min_variant_price.toLocaleString("vi-VN", {
-                                                style: "currency",
-                                                currency: "VND",
-                                            })}</span>
-                                    </span>
-                                }
-                            </div>
-                        </div>
-                        <div className='flex mt-auto justify-between items-center'>
-                            <div>
-                                <ModalProduct product={product} />
-                            </div>
-                            <div className='w-1/3 sm:w-1/6'>
-                                <NumberSpinner setCount={setCountProduct} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  const [countProduct, setCountProduct] = useState<number>(1);
+  return (
+    <div className="p-4 bg-white">
+      <div className="flex gap-4 py-3">
+        <Checkbox />
+        <div className="flex w-full gap-6">
+          <img
+            className="w-16 xl:w-28"
+            loading="lazy"
+            src={product.images[0].url}
+            alt=""
+          />
+          <div className="flex flex-col gap-3 w-full">
+            <div className="flex flex-col gap-2">
+              <div className="text-xs lg:text-base line-clamp-1 font-medium text-[#1c2430]">
+                {product.name}
+              </div>
+              <div className="flex text-xs lg:text-base gap-3">
+                <span
+                  className={`font-medium ${
+                    product.promotion_price != 0
+                      ? "text-[#e14337]"
+                      : "text-[#1c2430]"
+                  }`}
+                >
+                  {(
+                    product.min_variant_price - product.promotion_price
+                  ).toLocaleString("vi-VN", {
+                    style: "currency",
+                    currency: "VND",
+                  })}
+                </span>
+                {product.promotion_price != 0 && (
+                  <span className="font-medium text-[#9fb0c7] line-through">
+                    <span className="">
+                      {product.min_variant_price.toLocaleString("vi-VN", {
+                        style: "currency",
+                        currency: "VND",
+                      })}
+                    </span>
+                  </span>
+                )}
+              </div>
             </div>
+            <div className="flex mt-auto justify-between items-center">
+              <div>
+                <ModalProduct product={product} />
+              </div>
+              <div className="w-1/3 sm:w-1/6">
+                <NumberSpinner
+                  count={countProduct}
+                  setCount={setCountProduct}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
